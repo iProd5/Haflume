515fa68f669ab6cb67eb039fb5b171a391d106ee
iProd5 <franklinluther3@gmail.com>
2019-12-18 10:22:44 +0000

    components set

---
+++ src/app/retweet/retweet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-retweet',
+  templateUrl: './retweet.component.html',
+  styleUrls: ['./retweet.component.css']
+})
+export class RetweetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
